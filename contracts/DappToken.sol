68c84fffac49adaf5b6b9d81c6fed9b05fedecc3
sherifsk <sherif.s.elkhouly@gmail.com>
2019-07-28 18:38:12 +0100

    Initialises the token with correct values and allocates the total supply to admin account

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.5.8;
 
 contract DappToken {
-    //Constructor
-    //Set the total number of tokens
-    //Read the total number of tokens
-
+    string  public name = "DApp Token";
+    string  public symbol = "DAPP";
+    string  public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,14 +1,38 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappToken', function(accounts) {
-    
+    var tokenInstance;
+
+    it('initialises the token with the correct values', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(tokenName) {
+            assert.equal(tokenName, 'DApp Token', 'sets the token name to DApp Token');
+            return tokenInstance.symbol();
+        }).then(function(tokenSymbol) {
+            assert.equal(tokenSymbol, 'DAPP', 'sets the token symbol to DAPP');
+            return tokenInstance.standard();
+        }).then(function(tokenStandard) {
+            assert.equal(tokenStandard, 'DApp Token v1.0', 'sets the token standard to DApp Token v1.0');
+        });
+    });
+
     it('sets the total supply upon deployment', function() {
         return DappToken.deployed().then(function(instance) {
             tokenInstance = instance;
-
             return tokenInstance.totalSupply();
         }).then(function(totalSupply) {
-            assert.equal(totalSupply, 1000000, 'sets the total supply to 1,000,000')
+            assert.equal(totalSupply, 1000000, 'sets the total supply to 1,000,000');
+        });
+    });
+
+    it('allocates the total supply to the admin account', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance) {
+            assert.equal(adminBalance.toNumber(), 1000000, 'allocates the inital supply to the admin account');
         });
     });
 })
